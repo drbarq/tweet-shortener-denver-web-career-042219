3a0185f21d44cc69dee2d321c8ac673013231869
Joe Tustin <j.tustin@gmail.com>
2019-03-20 18:59:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,3 +37,18 @@ def bulk_tweet_shortener(array_of_tweets)
     puts word_substituter(tweet)
   end
 end
+
+
+
+def selective_tweet_shortener(array_of_tweets)
+  array_of_tweets.each do |tweet|
+    if tweet.length > 140 
+      puts word_substituter(tweet)
+    else 
+      puts tweet
+    end 
+  end 
+end 
+
+
+
